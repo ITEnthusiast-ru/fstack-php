2fb51ce3d9c7fbee89c394e38c38d51b55963004
Igor Bazanov <site-create-x@yandex.ru>
2025-02-23 21:09:59 +0700

    some work

--- www/src/footer.php
+++ www/src/footer.php
@@ -1,5 +1,5 @@
-<?php
 
-echo "Footer";
+<footer class="bg-dark text-white fixed-bottom text-center py-3">
+    <p>© 2024 Мастерская сайтов. Все права защищены.</p>
+</footer>
 
-?>
--- www/src/header.php
+++ www/src/header.php
@@ -1,5 +1,26 @@
-<?php
-
-echo "Заголовок";
-
-?>
+<nav class="py-2 bg-body-tertiary border-bottom">
+    <div class="container d-flex flex-wrap">
+      <ul class="nav me-auto">
+        <li class="nav-item"><a href="#" class="nav-link link-body-emphasis px-2 active" aria-current="page">Главная</a></li>
+        <li class="nav-item"><a href="#" class="nav-link link-body-emphasis px-2">Особенности</a></li>
+        <li class="nav-item"><a href="#" class="nav-link link-body-emphasis px-2">Цены</a></li>
+        <li class="nav-item"><a href="#" class="nav-link link-body-emphasis px-2">FAQ</a></li>
+        <li class="nav-item"><a href="#" class="nav-link link-body-emphasis px-2">О компании</a></li>
+      </ul>
+      <ul class="nav">
+        <li class="nav-item"><a href="#" class="nav-link link-body-emphasis px-2">Войти</a></li>
+        <li class="nav-item"><a href="#" class="nav-link link-body-emphasis px-2">Зарегистрироваться</a></li>
+      </ul>
+    </div>
+  </nav>
+  <header class="py-3 mb-4 border-bottom">
+    <div class="container d-flex flex-wrap justify-content-center">
+      <a href="/" class="d-flex align-items-center mb-3 mb-lg-0 me-lg-auto link-body-emphasis text-decoration-none">
+        <svg class="bi me-2" width="40" height="32"><use xlink:href="#bootstrap"/></svg>
+        <span class="fs-4">Double header</span>
+      </a>
+      <form class="col-12 col-lg-auto mb-3 mb-lg-0" role="search">
+        <input type="search" class="form-control" Placeholder="Поиск..." aria-label="Поиск">
+      </form>
+    </div>
+  </header>
